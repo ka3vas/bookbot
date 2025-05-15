19cbfb6d54461db4d71ec0d8f3f0f182ac7f3ad6
ka3vas <tyrnis@gmail.com>
2025-05-15 11:23:35 +0200

    added: sys

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # from file_name_without_extension import function_name
+import sys
 from stats import count_words, count_characters, sort_dict, generate_book_report
 
 def get_book_text(path):
@@ -6,7 +7,12 @@ def get_book_text(path):
 		return file.read()
 
 def main():
-	path = 'books/frankenstein.txt'
+	print(len(sys.argv))
+	if len(sys.argv) != 2:
+		print("Usage: python3 main.py <path_to_book>")
+		return sys.exit(1)
+	
+	path = sys.argv[1]
 
 	text = get_book_text(path)
 	num_words = count_words(text)
